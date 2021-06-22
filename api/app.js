8f5e371f248b22f7c70237ecf3820b0bc46c328f
Henry Zhang <zhangyiheng2003@gmail.com>
2021-06-21 21:21:32 -0400

    grugger

--- api/app.js
+++ api/app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +24,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const uri = "mongodb+srv://henry:henryz@cluster0.ksisl.mongodb.net/webapp?retryWrites=true&w=majority";
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology:true})
+
+console.log('Connected to Database')
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+const message = new mongoose.Schema({
+  message: String,
+})
+
+const Message = mongoose.model("Messages", message);
+
+usersRouter.post("/", function(req, res){
+  console.log("post");
+  const Receivedmessage = req.body.message;
+  const newMessageDocument = new Message({
+    message:Receivedmessage,
+  });
+  newMessageDocument.save();
+});
+module.exports = usersRouter;
 
 // error handler
 app.use(function(err, req, res, next) {
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+const axios = require('axios')
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message:""
+    }
+  }
+  handleChange=(event)=> {
+    this.setState({message:event.target.value});
+  }
+  handleClick=(event)=> {
+    axios.post("http://localhost:7000/users", {
+      message: this.state.message
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} value = {this.state.message} placeholder="enter a message"></input>
+        <button onClick = {this.handleClick}>Submit</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
